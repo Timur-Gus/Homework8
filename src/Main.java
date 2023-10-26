8d5fad918b31aed7d7283f5937d07d87106c004b
Timur-Gus <timurka24478@gmail.com>
2023-10-26 11:55:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        int[] arrInt = new int[3];
+        arrInt[0] = 1;
+        arrInt[1] = 2;
+        arrInt[2] = 3;
+        double[] arrDouble = {1.57, 7.654, 9.986};
+        long[] myArr = new long[10];
+        myArr[0] = 31;
+        myArr[8] = 12773;
+        myArr[9] = 3115801;
+    }
+}
