a39f1b0c058ec1ea04101a16c694a5c325c43bf4
Timur-Gus <timurka24478@gmail.com>
2023-10-26 12:17:29 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,29 @@ public class Main {
         myArr[0] = 31;
         myArr[8] = 12773;
         myArr[9] = 3115801;
+        // Задача 2
+        System.out.println("Задача 2");
+        System.out.println();
+        for (int i = 0; i < arrInt.length; i++){
+            if(i == (arrInt.length - 1)){
+                System.out.println(arrInt[i]);
+                break;
+            }
+            System.out.print(arrInt[i] + ", ");
+        }
+        for (int i = 0; i < arrDouble.length; i++){
+            if(i == (arrDouble.length - 1)){
+                System.out.println(arrDouble[i]);
+                break;
+            }
+            System.out.print(arrDouble[i] + ", ");
+        }
+        for (int i = 0; i < myArr.length; i++){
+            if(i == (myArr.length - 1)){
+                System.out.println(myArr[i]);
+                break;
+            }
+            System.out.print(myArr[i] + ", ");
+        }
     }
 }
